b9bc5fb45943404f8f302d2361628e7d305c8f70
thomas simm <simmhthomas@gmail.com>
2022-02-28 23:08:32 +0000

    Update GolfSwingApp.py

--- GolfSwingApp.py
+++ GolfSwingApp.py
@@ -63,11 +63,11 @@ def load_data(choi):
 
     predictions=model(imgTens)
     
-    return predictions,imgLocAll,cda2, imgs
+    return predictions,imgLocAll,cda2, imgs,imgAll
 
 data_load_state = st.text('Loading data...')
 useME = 3
-predictions,imgLocAll,cda2, imgs=load_data(useME)
+predictions,imgLocAll,cda2, imgs, imgAll=load_data(useME)
 data_load_state.text("Loaded data (using st.cache)")
 
 
@@ -81,6 +81,7 @@ imgSEL = st.sidebar.selectbox(
 imgLocAll
 useME
 imgs
+imgAll[0:10]
 
 img = mpimg.imread(cda2+'/images/'+imgSEL)
 
